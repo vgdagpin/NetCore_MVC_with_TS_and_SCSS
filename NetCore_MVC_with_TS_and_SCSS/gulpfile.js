779613cee57b4975c5127f59a49617c3dbc5074c
Vincent Dagpin <vincent.dagpin@gmail.com>
2023-10-25 15:21:51 +0800

    Init

---
+++ NetCore_MVC_with_TS_and_SCSS/gulpfile.js
@@ -0,0 +1,183 @@
+const gulp = require('gulp');
+const gulp_sass = require('gulp-sass');
+const gulp_typescript = require('gulp-typescript');
+const sass = require('gulp-sass')(require('sass'));
+const sourcemaps = require('gulp-sourcemaps');
+const csso = require('gulp-csso');
+const cleanCSS = require('gulp-clean-css'); // no need, using csso
+const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
+const concat = require('gulp-concat');
+const flatten = require('gulp-flatten');
+const replace = require('gulp-replace');
+
+let siteCSS = [
+    './wwwroot/scss/site.scss'
+];
+
+// build kendo.scss
+gulp.task('build-kendo', () => (
+    gulp.src('./wwwroot/scss/kendo.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./wwwroot/css'))
+));
+
+// build kendo.scss (minified)
+gulp.task('build-kendo-minified', () => (
+    gulp.src('./wwwroot/scss/kendo.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(csso())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('./wwwroot/css'))
+));
+
+// build site.scss
+gulp.task('build-site', () => (
+    gulp.src(siteCSS)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(concat('site.css'))
+        .pipe(gulp.dest('./wwwroot/css'))
+));
+
+// build site.scss (minified)
+gulp.task('build-site-minified', () => (
+    gulp.src(siteCSS)
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(concat('site.css'))
+        .pipe(csso())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('./wwwroot/css'))
+));
+
+// build ts/modules/*.ts
+function buildTsModules(env) {
+    var autoGenText = '/* This is auto generated, go to Task Runner Explorer and run bundleJS */\n\n\n';
+
+    // get the _tsconfig.json, we need to do this to control the ordering of modules
+    // when we concat all modules into 1 file
+    var ts = gulp_typescript.createProject('./wwwroot/ts/modules/_tsconfig.json');
+
+    // create the pipeline
+    var pipeline = ts.src()
+        .pipe(ts())
+        //.pipe(sourcemaps.init())
+        .js
+        .pipe(replace(/^import\s.*from.*$/gm, (match) => `// ${match}`))
+        .pipe(replace(/export /gm, ''))
+        .pipe(concat('site-modules.js'))
+        ;
+
+    if (env == 'Production') {
+        pipeline = pipeline
+            .pipe(uglify())
+            .pipe(rename({ suffix: '.min' }))
+            ;
+    }
+
+    pipeline = pipeline
+        .pipe(replace(/^/, autoGenText))
+        //.pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('./wwwroot/js'));
+
+    return pipeline;
+}
+
+// build ts/*.ts except modules/*.ts
+function buildTs(env) {
+    var pipeline = gulp.src([
+        './wwwroot/ts/**/*.ts',
+        '!./wwwroot/ts/modules/*.ts'
+    ])
+        //.pipe(sourcemaps.init())
+        .pipe(gulp_typescript({
+            noImplicitAny: false,
+            target: 'es6',
+            sourceMap: true
+        }))
+        .js
+        .pipe(replace(/^import\s.*from.*$/gm, (match) => `// ${match}`))
+        .pipe(replace(/export /gm, ''))
+        .pipe(concat('site.js'))
+        ;
+
+    if (env == 'Production') {
+        pipeline = pipeline
+            .pipe(uglify())
+            .pipe(rename({ suffix: '.min' }))
+            ;
+    }
+
+    pipeline = pipeline
+        .pipe(replace(/^/, '/* This is auto generated, go to Task Runner Explorer and run bundleJS */\n\n\n'))
+        //.pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('./wwwroot/js'));
+
+    return pipeline;
+}
+
+// build *.ts under .cshtml
+function buildCSHTMLTs(env) {
+    var pipeline = gulp.src([
+        './Areas/**/*.ts',
+        './Views/**/*.ts'
+    ])
+        .pipe(sourcemaps.init())
+        .pipe(gulp_typescript({
+            noImplicitAny: false,
+            target: 'es6',
+            sourceMap: true,
+            rootDir: './'
+        }))
+        .js
+        .pipe(replace(/^import\s.*from.*$/gm, (match) => `// ${match}`))
+        .pipe(replace(/^export /gm, (match) => `// ${match}`))
+        .pipe(rename(function (path) {
+            path.basename = path.basename.replace('.cshtml', '');
+        }))
+        ;
+
+    if (env == 'Production') {
+        pipeline = pipeline
+            .pipe(uglify())
+            .pipe(rename({ suffix: '.min' }))
+            ;
+    }
+
+    pipeline = pipeline
+        .pipe(replace(/^/, '/* This is auto generated, go to Task Runner Explorer and run bundleJS */\n\n\n'))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('./wwwroot/js'));
+
+    return pipeline;
+}
+
+gulp.task('build-ts', () => buildTs(/*'Production'*/));
+gulp.task('build-cshtml-ts', () => buildCSHTMLTs(/*'Production'*/));
+gulp.task('build-ts-modules', () => buildTsModules(/*'Production'*/));
+
+exports.$bundleCSS = gulp.series
+    (
+        'build-kendo',
+        'build-aerish'
+    );
+
+exports.$bundleJS = gulp.series
+    (
+        'build-ts-modules',
+        'build-ts',
+        'build-cshtml-ts'
+    );
+
+exports.$bundleMinify = gulp.series
+    (
+        //'build-kendo',
+        'build-kendo-minified',
+        //'build-aerish',
+        'build-aerish-minified',
+        //'build-ts',
+        //'build-ts-minified'
+    );
---
+++ ReadMe.md
@@ -0,0 +1,36 @@
+1. Right click project
+1. Open in Terminal
+1. npm install gulp gulp-webpack webpack webpack-cli --save-dev
+1. npm install node-sass --save-dev
+1. add wwwroot/scss/_variables.scss
+```
+$bodyFontSize: 1rem;
+$bodyFontFamily: 'Roboto', sans-serif;
+```
+1. add wwwroot/scss/kendo.scss
+```
+@import "./_variables.scss";
+
+@import "node_modules/@progress/kendo-theme-default/dist/all.scss";
+
+@import "./kendo.grid.scss";
+@import "./kendo.panelbar.scss";
+```
+1. add wwwroot/scss/site.scss
+1. add wwwroot/ts/modules/_tsconfig.json
+```
+{
+  "compilerOptions": {
+    "module": "es6", // Set module to "es6"
+    "noImplicitAny": false,
+    "target": "es6",
+    "sourceMap": true
+  },
+  "include": [
+    "module1.ts"
+  ]
+}
+```
+1. add wwwroot/ts/modules/module1.ts
+1. add wwwroot/ts/site.ts
+1. add gulpfile.js
